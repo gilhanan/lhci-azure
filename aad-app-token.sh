eba7df3d7b3a78dfc543993b07c169137a62b889
Gil Hanan <gil.hanan@gmail.com>
2023-08-18 07:16:30 +0300

    update aad-app-token

--- aad-app-token.sh
+++ aad-app-token.sh
@@ -4,4 +4,9 @@ TENANT_ID=
 
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
 
-az account get-access-token --resource https://ossrdbms-aad.database.windows.net --query accessToken -o tsv
+password=$(az account get-access-token --resource https://ossrdbms-aad.database.windows.net --query accessToken -o tsv)
+
+mysql --host gh-lhci-server-db.mysql.database.azure.com \
+      --user lhci \
+      --password=$password \
+      --enable-cleartext-plugin
