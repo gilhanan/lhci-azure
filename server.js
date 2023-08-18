76e1f404e5d0e09a78671027e3da3c89a91f67aa
Gil Hanan <gil.hanan@gmail.com>
2023-08-18 07:20:40 +0300

    Locall

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const { createServer } = require("@lhci/server");
-const {
-  useIdentityPlugin,
-  DefaultAzureCredential,
-} = require("@azure/identity");
+const { DefaultAzureCredential } = require("@azure/identity");
 
 const {
-  IDENTITY_ENDPOINT,
-  ENVIRONMENT: environment,
   MYSQL_SERVER_NAME: mysqlServerName,
   MYSQL_DATABASE_NAME: mysqlDatabaseName,
   SQL_DATABASE_PATH: sqlDatabasePath,
   SQL_DIALECT: sqlDialect,
   WEBSITES_PORT: port,
 } = process.env;
-
-if (environment === "development") {
-  const { vsCodePlugin } = require("@azure/identity-vscode");
-  useIdentityPlugin(vsCodePlugin);
-}
 
 async function getAccessToken() {
   console.log("Getting Managed Identity credentials...");
@@ -32,14 +22,10 @@ async function getAccessToken() {
   return token;
 }
 
-function getUser() {
-  return IDENTITY_ENDPOINT ? "lhci" : "lhci@gilhanangmail.onmicrosoft.com";
-}
-
 async function startServer() {
   console.log("Starting server...");
 
-  const user = getUser();
+  const user = "lhci";
   const password = await getAccessToken();
 
   const sqlConnectionUrl = `mysql://${user}:${password}@${mysqlServerName}.mysql.database.azure.com:3306/${mysqlDatabaseName}`;
